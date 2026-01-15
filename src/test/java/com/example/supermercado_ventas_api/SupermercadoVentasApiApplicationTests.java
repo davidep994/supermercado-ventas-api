1b3c39c8842b9898cab9049cfc4984c0814f41d4
Edu <egarciadevesa@gmail.com>
2026-01-15 20:17:01 +0100

    Primer commit

---
+++ src/test/java/com/example/supermercado_ventas_api/SupermercadoVentasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supermercado_ventas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermercadoVentasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
